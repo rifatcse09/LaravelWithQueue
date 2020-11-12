77d3a7c9051cb433c2d06d264a7bd920b59bb651
rifatcse09 <rifatcse09@gmail.com>
2020-11-12 21:17:49 +0600

    redis with que

--- app/Jobs/SendOrderEmail.php
+++ app/Jobs/SendOrderEmail.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\OrderShipped;
 use App\Order;
@@ -36,8 +37,20 @@ class SendOrderEmail implements ShouldQueue
 	 */
 	public function handle()
 	{
-		$recipient = 'steven@example.com';
-		Mail::to($recipient)->send(new OrderShipped($this->order));
-		Log::info('Emailed order ' . $this->order->id);
+		// $recipient = 'steven@example.com';
+		// Mail::to($recipient)->send(new OrderShipped($this->order));
+		// Log::info('Emailed order ' . $this->order->id);
+
+		 // Allow only 2 emails every 1 second
+        Redis::throttle('any_key')->allow(2)->every(1)->then(function () {
+
+            $recipient = 'hello@example.com';
+            Mail::to($recipient)->send(new OrderShipped($this->order));
+            Log::info('Emailed order ' . $this->order->id);
+
+        }, function () {
+            // Could not obtain lock; this job will be re-queued
+            return $this->release(2);
+        });
 	}
 }
